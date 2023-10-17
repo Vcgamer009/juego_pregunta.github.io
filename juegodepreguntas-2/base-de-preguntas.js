b4c22964b6d472d4487e1b800ff1ebedc40bb9e4
Vcgamer009 <147891075+Vcgamer009@users.noreply.github.com>
2023-10-17 00:47:03 -0300

    initial commit

---
+++ juegodepreguntas-2/base-de-preguntas.js
@@ -0,0 +1,35 @@
+let baseDePreguntas = [
+    {
+        pregunta: "¿En qué siglo se inició la Revolución Industrial?",
+        ayuda: "Aproximadamente hacia el año 1760, concluyó en la década de 1840 y se limitó principalmente a Gran Bretaña.",
+        ayudaImg: "",
+        imagen: "https://i.ibb.co/J2tWc2f/BASF-Werk-Ludwigshafen-1881.jpg",
+        respuesta: " XVIII",
+        distractores: ["XIV", "XV", "XX"],
+    },
+    {
+        pregunta: "¿Cuál fue el primer movimiento social feminista que surgió en el Paraguay?",
+        ayuda:"Nació a sugerencia del diputado republicano Telémaco Silvera en 1920",
+        imagen: "",
+        respuesta: "Centro Feminista Paraguayo",
+        distractores: ["Asociación Feminista", "Consejo de Mujeres del Paraguay", "Unión Feminista del Paraguay"],
+    },
+    {
+        pregunta: "¿Cuál de estas obras son las del Gobierno de Bernardino Caballero?",
+        ayuda: "",
+        imagen: "https://i.ibb.co/12C2M9C/Bernardino-Caballero.jpg",
+        respuesta: "Creó el Registro Civil de las Personas (1890), La Escuela de Derecho (1884)",
+        distractores: ["Creó La ley de Contribucion Directa", "Se fortableció la Guardia Nacional", "Se sancionó la Ley del Matrimoni Civil, en 1898"],
+    },
+    {
+        pregunta: "¨Son formas de participación de la ciudadanía en la sociedad, que se organiza para actuar colectivamente e impulsar el logro de objetivos comunes¨. Corresponde a:",
+        respuesta: "Movimientos Sociales",
+        distractores: ["Drotinas Sociales", "Nacionalismo", "Proletario"],
+    },
+    {
+        pregunta: "En el Gobierno de quién ¨se fundaron los partidos políticos: el Centro Democrático y la Asociación Nacional Republicana (1887)¨",
+        ayuda: "Fue presidente del Paraguay desde el 25 de noviembre de 1886 hasta el 25 de noviembre de 1890",
+        respuesta: "Patricio Escobar",
+        distractores: ["Emilio Aceval", "Manuel Domingiez", "Juan Bautista Egusquiza"],
+    },
+]
